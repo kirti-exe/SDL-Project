9c8ec10fc620b5c4f00385bcd7097f41e1c43228
kirti-exe <kirti.exe@gmail.com>
2026-03-07 05:37:09 +0530

    create window with sdl window and renderer function by using createWindowAndRenderer with res of 640x480 pixels and giving it a title this is pretty basic nothing much, these all are made in main.cpp, graphics.h header file, and graphics.cpp. ill add game.cpp later on this day or maybe next day we'll see"

---
+++ src/graphics.cpp
@@ -0,0 +1,17 @@
+#include <sdl/SDL.h>
+
+#include "graphics.h"
+
+/* Graphics class
+ *  Holds all information dealing  with graphics for the game
+ */
+
+Graphics::Graphics(){
+    SDL_CreateWindowAndRenderer(640, 480, 0, &this->_window, &this->_renderer);
+    SDL_SetWindowTitle(this->_window, "CaveStory");
+}
+
+Graphics::~Graphics(){
+    SDL_DestroyWindow(this->_window);
+    SDL_DestroyRenderer(this->_renderer);
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#define SDL_MAIN_HANDLED
+#include <sdl/SDL.h>
+#include "graphics.h"
+
+int main(int argc, const char* argv[]){
+    Graphics graphics;
+    while(true){
+
+    }
+    return 0;
+}
